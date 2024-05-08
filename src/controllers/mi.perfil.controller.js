91e37f3c5dd22678f3cf2b89551ad46cc311fbb7
Leandro <andrianileandro@hotmail.com>
2024-05-07 21:17:00 -0300

    Add editar perfil

--- src/controllers/mi.perfil.controller.js
+++ src/controllers/mi.perfil.controller.js
@@ -1,26 +1,65 @@
+import { db } from '@vercel/postgres'
+import axios from 'axios'
 
 export const getMiPerfil = async (req, res) => {
     
-    if(req.signedCookies['loggedin']){
+    const client = await db.connect()
+    let compras
+    let pedidos
 
-        const usuarioSesion = {
-            nombres: req.signedCookies['nombres'],
-            apellidos: req.signedCookies['apellidos'],
-            img_url: req.signedCookies['img_url'],
-            rol: req.signedCookies['rol']
-        }
-
-        res.render('miPerfil', {
-         login: true,
-         id: req.signedCookies['idUser'],
-         usuarioSesion
-        })
-
-    } else {
-
-        res.redirect('/')
+    try {
+    
+        // Consulta SQL
+        const consulta = `SELECT email FROM usuarios_1 WHERE usuario_id = ${req.signedCookies['idUser']}`;
+        const consulta1 = `SELECT * FROM compras WHERE usuario_id = ${req.signedCookies['idUser']}`;
         
-    } 
+        // Ejecutar la consulta
+        const resultado = await client.query(consulta);
+        const resultado1 = await client.query(consulta1);
+        compras = resultado1.rows
+        
+        for(let i = 0; i < compras.length; i++) {
+            const consulta2 = `SELECT * FROM pedidos WHERE compra_id = ${compras[i].compra_id}`;
+            const resultado2 = await client.query(consulta2);
+            compras[i].pedidos = resultado2.rows
+        }   
+
+        console.log('Usuario:', resultado.rows[0]); // Imprimir los resultados
+        console.log('Compras:', compras)
+
+         
+
+
+        if(req.signedCookies['loggedin']){
+
+            const usuarioSesion = {
+                nombres: req.signedCookies['nombres'],
+                apellidos: req.signedCookies['apellidos'],
+                img_url: req.signedCookies['img_url'],
+                rol: req.signedCookies['rol']
+            }
+
+            res.render('miPerfil', {
+            login: true,
+            id: req.signedCookies['idUser'],
+            usuarioSesion,
+            email: resultado.rows[0].email,
+            compras
+            })
+
+        } else {
+
+            res.redirect('/')
+            
+        } 
+          
+
+      } catch (error) {
+        console.error('Error al consultar datos de usuario:', error);
+        res.status(500).json({ error: 'Error al consultar datos de usuario' });
+      }
+
+    
     
 
 }
---
+++ src/public/js/miPerfil.js
@@ -0,0 +1,110 @@
+
+function editarPerfil(){
+
+}
+
+document.getElementById('formModal').addEventListener('submit', function(event) {
+    event.preventDefault()
+})
+
+let element
+let image 
+let image64 = ''
+document.getElementById('image').addEventListener('change', () => {
+    image = document.getElementById('image').files[0]
+    let reader= new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload=()=>{
+        //aqui ya esta en base64
+        image64 = reader.result;
+    }
+    objectURL = URL.createObjectURL(image)
+    document.getElementById('imagePrevia').innerHTML = `
+    <img src="${objectURL}">
+    `
+})
+
+function cancelarForm(){
+    document.getElementById('fondoModal').style.display = 'none'
+    vaciarModal()
+}
+
+document.getElementById('fondoModal').addEventListener('click', function(event) {
+    if (event.target === document.getElementById('fondoModal')) {
+        cancelarForm()
+    }
+})
+
+function editarPerfil(){
+    document.getElementById('fondoModal').removeAttribute('style')
+    document.getElementById('imagePrevia').innerHTML = `
+    <img src="${document.getElementById('img_url').src}">
+    `
+    document.getElementById('nombre').value = document.getElementById('nombreUser').innerHTML
+    document.getElementById('apellido').value = document.getElementById('apellidoUser').innerHTML
+    document.getElementById('email').value = document.getElementById('emailUser').innerHTML
+
+}
+
+function vaciarModal(){
+    document.getElementById('nombre').value = ''
+    document.getElementById('apellido').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('image').value = ''
+    image64 = ''
+}
+
+function guardarForm(){  
+
+    let nombre = document.getElementById('nombre').value
+    let apellido = document.getElementById('apellido').value
+    let email = document.getElementById('email').value
+
+
+    if( nombre !== '' && apellido !== '' && email !== '' ){
+
+        if(image64 !== '') {
+            let user = {
+            nombre,
+            apellido,
+            email,
+            image64
+            }
+            putForm(user)
+            cancelarForm() 
+        }else{
+            let user = {
+                nombre,
+                apellido,
+                email
+            }
+            putForm(user)
+            cancelarForm()
+        }
+        
+        
+    }
+}
+
+function putForm(user){
+    let url = '/admin/productos'
+    let request = new Request(url, {
+        method: 'PUT',
+        body: JSON.stringify(producto),
+        headers: {
+            /* "Content-Type": "multipart/form-data", */
+            "Content-Type": "application/json",            
+            /* "Content-Type": "application/x-www-form-urlencoded", */
+        }, 
+    });
+    fetch(request)
+    .then(response => response.json())
+    .then( (data) => {
+        if(data === 1){
+            console.log('Se ha editado correctamente')
+            actualizarCards()
+        }else if(data.error){
+            console.log('No se ha editado el producto')
+        }
+    })
+}
