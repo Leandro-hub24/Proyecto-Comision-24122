eaeb4854671abaa9bf5961f6dec321e7d35b8a62
Leandro <andrianileandro@hotmail.com>
2024-05-11 21:05:10 -0300

    Update diseño

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -29,44 +29,46 @@ function postFetch() {
     //agregar funcion para ver si los input estan vacios
 
     const url = '/login';
-
-    let form = {
+        let form = {
         email: document.getElementById('email').value,
         pass: document.getElementById('pass').value
-    }
-    var request = new Request(url, {
-        method: 'POST',
-        body: JSON.stringify(form),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    fetch(request)
-    .then(res => res.json())
-    .then (data => {
-
-        if(data.login){
-
-            Swal.fire({
-            icon: "success", 
-            title: data.msg,
-            footer: '<button class="btt" ><a href="/" >Home</a></button>',
-            showConfirmButton: false    
-            })
-          setTimeout(function () {
-            window.location = '/'
-          }, 3000)
-
-        }else{
-
-            Swal.fire({
-                icon: "error", 
-                title: data.msg,
-            })
-
         }
-        
-    })
+        var request = new Request(url, {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        fetch(request)
+        .then(res => res.json())
+        .then (data => {
+
+            if(data.login){
+
+                Swal.fire({
+                icon: "success", 
+                title: data.msg,
+                footer: '<button class="btt" ><a href="/" >Home</a></button>',
+                showConfirmButton: false    
+                })
+            setTimeout(function () {
+                window.location = '/'
+            }, 3000)
+
+            }else{
+
+                Swal.fire({
+                    icon: "error", 
+                    title: data.msg,
+                })
+
+            }
+            
+        })
+
+
+    
 }
 
 function registro(){
@@ -75,20 +77,33 @@ function registro(){
 
 document.getElementById('submit').addEventListener('click', () => {
     if(validarEmail(document.getElementById('email').value)){
+        document.getElementById('email').style.borderColor = 'black'
         if(document.getElementById('pass').value !== ''){
+            document.getElementById('password').style.borderColor = 'black'
             document.getElementById('submit').disabled = 'true'
             postFetch()
         }else{
-            Swal.fire({
+            /* Swal.fire({
                 icon: "error", 
                 title: 'Inserte una contraseña valida',
-            })
+            }) */
+            document.getElementById('errorPass').removeAttribute('style')
+            document.getElementById('password').style.borderColor = 'red'
+            setTimeout(function () {
+                document.getElementById('errorPass').style.display = 'none'
+            }, 3000)
+
         }
     }else{
-        Swal.fire({
+        /* Swal.fire({
             icon: "error", 
             title: 'Inserte un email valido',
-        })
+        }) */
+        document.getElementById('errorEmail').removeAttribute('style')
+        document.getElementById('email').style.borderColor = 'red'
+        setTimeout(function () {
+            document.getElementById('errorEmail').style.display = 'none'
+        }, 3000)
     }
     
 })
