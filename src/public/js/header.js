3af73a0f6830363d5b081d218532e230aafa1e44
Leandro <andrianileandro@hotmail.com>
2024-05-12 12:29:35 -0300

    Update header responsive

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -6,3 +6,11 @@ document.getElementById('imageUser').addEventListener('click', () => {
         document.getElementById('drop-down').classList.add('show-drop-down')
     }
 })
+
+document.getElementById('hamburger').addEventListener('click', () => {
+    if(document.getElementById('drop-down1').classList.contains('show-drop-down1')){
+        document.getElementById('drop-down1').classList.remove('show-drop-down1')
+    }else{
+        document.getElementById('drop-down1').classList.add('show-drop-down1')
+    }
+})
