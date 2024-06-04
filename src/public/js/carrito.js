047998c8c9479803e45913a8b6ce175dd92c0e78
Leandro <andrianileandro@hotmail.com>
2024-06-04 16:29:25 -0300

    Add carrito en todas las páginas

--- src/public/js/carrito.js
+++ src/public/js/carrito.js
@@ -29,7 +29,7 @@ function showCarritoSave() {
         document.getElementById('cardsCarrito').innerHTML += `
                         <div class='cardCarrito' id="card-${compra.producto_id}">
                             <div class="izq">
-                                <div class="img">
+                                <div class="imgC">
                                     <img src="${compra.img_url}" alt="Img">
                                 </div>
                                 <div class="infoPro">
@@ -53,6 +53,16 @@ function showCarritoSave() {
 
     
                     
+}
+
+function actualizarCarrito() {
+    document.getElementById('cant').innerHTML = cantCarrito
+    document.getElementById('precioTotal').innerHTML = formatear(`${precioCarrito}`)
+    document.getElementById('totalCarrito').innerHTML = formatear(`${precioCarrito}`)
+    localStorage.clear()
+    localStorage.setItem("carrito", JSON.stringify(compra))
+    localStorage.setItem("cantCarrito", JSON.stringify(cantCarrito))
+    localStorage.setItem("precioCarrito", JSON.stringify(precioCarrito))
 }
 
 function formatear(i) {
@@ -81,69 +91,7 @@ document.getElementById('open').addEventListener('click', () => {
     }, 100)
 })
 
-function addCarrito(id) {
 
-    if(compra.find(elemento => elemento.producto_id === id) === undefined) {
-        let url = `/productosCarrito/${id}`
-        fetch(url)
-            .then(res => res.json())
-            .then((producto) => {
-                document.getElementById('cardsCarrito').innerHTML += `
-                        <div class='cardCarrito' id="card-${producto[0].producto_id}">
-                            <div class="izq">
-                                <div class="img">
-                                    <img src="${producto[0].img_url}" alt="Img">
-                                </div>
-                                <div class="infoPro">
-                                    <h5>${producto[0].nombre}</h5>
-                                    <p>${producto[0].descripcion}</p>
-                                </div>
-                                
-                            </div>
-                            <div class="med">
-                                <i class='bx bxs-x-square' onclick="quitarCarrito(${producto[0].producto_id})"></i>
-                            </div>
-                            <div class="der">
-                                <p>$<span id="precioCant-${producto[0].producto_id}">${formatear(`${producto[0].precio}`)}</span></p>
-                                <input type="number" name="cantProd" id="cantProd-${producto[0].producto_id}" value="1" oninput="inputCant(${producto[0].producto_id})">
-                                
-                            </div>
-                        </div>
-                    `
-                let i = {
-                    nombre: producto[0].nombre,
-                    descripcion: producto[0].descripcion,
-                    img_url: producto[0].img_url,
-                    producto_id: producto[0].producto_id,
-                    precio: producto[0].precio,
-                    cantidad: 1
-                }
-                compra.push(i)
-                cantCarrito += 1
-                precioCarrito += producto[0].precio
-                actualizarCarrito()
-            })
-    } else {
-        compra[index(compra, id)].cantidad += 1
-        cantCarrito += 1
-        precioCarrito += compra[index(compra, id)].precio
-        document.getElementById(`precioCant-${id}`).innerHTML = formatear(`${compra[index(compra, id)].precio * compra[index(compra, id)].cantidad}`)
-        let j = document.getElementById(`cantProd-${id}`).value
-        document.getElementById(`cantProd-${id}`).value = Number(j) + Number(1)
-        actualizarCarrito()
-    }
-
-}
-
-function actualizarCarrito() {
-    document.getElementById('cant').innerHTML = cantCarrito
-    document.getElementById('precioTotal').innerHTML = formatear(`${precioCarrito}`)
-    document.getElementById('totalCarrito').innerHTML = formatear(`${precioCarrito}`)
-    localStorage.clear()
-    localStorage.setItem("carrito", JSON.stringify(compra))
-    localStorage.setItem("cantCarrito", JSON.stringify(cantCarrito))
-    localStorage.setItem("precioCarrito", JSON.stringify(precioCarrito))
-}
 
 const filtro = (lista, id) => {
     return lista.filter(elemento => elemento.producto_id !== id);
