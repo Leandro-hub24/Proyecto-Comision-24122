44c4729190acbe7ab72a4fff99f1b25845d6f3f5
Leandro <andrianileandro@hotmail.com>
2024-06-04 14:55:10 -0300

    Update carrito add localstorage

--- src/public/js/carrito.js
+++ src/public/js/carrito.js
@@ -1,3 +1,60 @@
+let compra = new Array
+let cantCarrito = 0
+let precioCarrito = 0
+
+window.addEventListener('load', () => {
+    if(localStorage){
+        if(localStorage.getItem('carrito') !== undefined && localStorage.getItem('carrito')){
+            compra = JSON.parse(localStorage.getItem('carrito'))
+            showCarritoSave()
+        }
+    }
+})
+
+function showCarritoSave() {
+
+    if(localStorage.getItem('cantCarrito') !== undefined && localStorage.getItem('cantCarrito')) {
+        cantCarrito = JSON.parse(localStorage.getItem('cantCarrito'))
+    }
+
+    if(localStorage.getItem('precioCarrito') !== undefined && localStorage.getItem('precioCarrito')) {
+        precioCarrito = JSON.parse(localStorage.getItem('precioCarrito'))
+    }
+    
+    
+    actualizarCarrito()
+
+    compra.map( (compra) => {
+
+        document.getElementById('cardsCarrito').innerHTML += `
+                        <div class='cardCarrito' id="card-${compra.producto_id}">
+                            <div class="izq">
+                                <div class="img">
+                                    <img src="${compra.img_url}" alt="Img">
+                                </div>
+                                <div class="infoPro">
+                                    <h5>${compra.nombre}</h5>
+                                    <p>${compra.descripcion}</p>
+                                </div>
+                                
+                            </div>
+                            <div class="med">
+                                <i class='bx bxs-x-square' onclick="quitarCarrito(${compra.producto_id})"></i>
+                            </div>
+                            <div class="der">
+                                <p>$<span id="precioCant-${compra.producto_id}">${formatear(`${compra.precio * compra.cantidad}`)}</span></p>
+                                <input type="number" name="cantProd" id="cantProd-${compra.producto_id}" value="${compra.cantidad}" oninput="inputCant(${compra.producto_id})">
+                                
+                            </div>
+                        </div>
+                    `
+
+    })
+
+    
+                    
+}
+
 function formatear(i) {
     const decimal = Number(i.replace(",", ".")).toFixed(2); //cambiamos la "," por el "." por si introduce un string con ","
     const formated = decimal.replace(".", ",").replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -24,10 +81,6 @@ document.getElementById('open').addEventListener('click', () => {
     }, 100)
 })
 
-let compra = new Array
-let cantCarrito = 0
-let precioCarrito = 0
-
 function addCarrito(id) {
 
     if(compra.find(elemento => elemento.producto_id === id) === undefined) {
@@ -35,7 +88,6 @@ function addCarrito(id) {
         fetch(url)
             .then(res => res.json())
             .then((producto) => {
-                // agregar condicional por si se repite el producto 
                 document.getElementById('cardsCarrito').innerHTML += `
                         <div class='cardCarrito' id="card-${producto[0].producto_id}">
                             <div class="izq">
@@ -59,6 +111,9 @@ function addCarrito(id) {
                         </div>
                     `
                 let i = {
+                    nombre: producto[0].nombre,
+                    descripcion: producto[0].descripcion,
+                    img_url: producto[0].img_url,
                     producto_id: producto[0].producto_id,
                     precio: producto[0].precio,
                     cantidad: 1
@@ -78,14 +133,16 @@ function addCarrito(id) {
         actualizarCarrito()
     }
 
-    
-
 }
 
 function actualizarCarrito() {
     document.getElementById('cant').innerHTML = cantCarrito
     document.getElementById('precioTotal').innerHTML = formatear(`${precioCarrito}`)
     document.getElementById('totalCarrito').innerHTML = formatear(`${precioCarrito}`)
+    localStorage.clear()
+    localStorage.setItem("carrito", JSON.stringify(compra))
+    localStorage.setItem("cantCarrito", JSON.stringify(cantCarrito))
+    localStorage.setItem("precioCarrito", JSON.stringify(precioCarrito))
 }
 
 const filtro = (lista, id) => {
@@ -105,7 +162,7 @@ function quitarCarrito(id) {
     cantCarrito -= compra[index(compra, id)].cantidad
     precioCarrito -= (compra[index(compra, id)].precio * compra[index(compra, id)].cantidad)
 
-    compra = filtro(compra)
+    compra = filtro(compra, id)
     actualizarCarrito()
 }
 
@@ -128,5 +185,4 @@ function inputCant(id) {
         }
     }
 
-    //comprarar con el array de compra para saber si la cantidad sube o disminuye
 }
